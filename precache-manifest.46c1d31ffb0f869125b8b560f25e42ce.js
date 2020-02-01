b87a1d1b5056d121e7a4ab0e0c993c8e0daeba7a
Ben Taussig <bpt28@cornell.edu>
2020-02-01 10:35:04 -0500

    Updates

---
+++ precache-manifest.46c1d31ffb0f869125b8b560f25e42ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2835d36cf89a1cbb39bd567166f74d22",
+    "url": "/kitchen_duty_frontend/index.html"
+  },
+  {
+    "revision": "486d20f176ea78736d98",
+    "url": "/kitchen_duty_frontend/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "976081a755b29cf8a5b0",
+    "url": "/kitchen_duty_frontend/static/css/main.3ce2cfb1.chunk.css"
+  },
+  {
+    "revision": "486d20f176ea78736d98",
+    "url": "/kitchen_duty_frontend/static/js/2.b0c09b95.chunk.js"
+  },
+  {
+    "revision": "334b4b987003d90b9c311ba0ca8dd3aa",
+    "url": "/kitchen_duty_frontend/static/js/2.b0c09b95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976081a755b29cf8a5b0",
+    "url": "/kitchen_duty_frontend/static/js/main.0de677e1.chunk.js"
+  },
+  {
+    "revision": "9cc0ddb8566b98d963b3",
+    "url": "/kitchen_duty_frontend/static/js/runtime-main.f44d4f1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kitchen_duty_frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
